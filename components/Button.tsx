6ea8371ef79af5edeea5582aea332fdb10359669
xomcodes <xommyb@gmail.com>
2023-04-15 13:41:54 +0100

    initial commit

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,3 +1,34 @@
-export default function Button(){
-  return  <button style={{}}>Click</button>
+import { Icon } from "@iconify/react";
+
+type Props = {
+  disableShadow?: boolean;
+  disabled?: boolean;
+  variant?: "text" | "button",
+  size?: "sm" | "md" | "lg",
+  startIcon?: JSX.Element,
+  endIcon?: JSX.Element
+}
+
+export default function Button({ disableShadow, disabled, variant, size, startIcon, endIcon,}: Props) {
+  return (
+    <button
+      disabled={disabled ? true : false}
+      style={{
+        backgroundColor: disabled ? "#e0e0e0" : "blue",
+        padding: "9px 16px",
+        borderRadius: "5px",
+        color: disabled ? "#9e9e9e" : "white",
+        border: "none",
+        boxShadow: disableShadow ? "none" : "-1px 10px 17px -11px rgba(0,0,0,0.75)",
+        fontSize: "15px",
+        display: "flex",
+        alignItems: "center",
+        gap: "2px",
+        cursor: disabled ? "default" : "pointer"
+      }}
+    >
+      <Icon icon="ic:sharp-add-shopping-cart" />
+      Button
+    </button>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Sidebar from '../components/sidebar'
+import Button from '@/components/Button'
 
 function index() {
   return (
-    <div>
-      <Sidebar />
-    </div>
+    <form style={{display: "flex", flexDirection: "column", gap: "20px", alignItems: "start"}}>
+      <Button disableShadow />
+      <Button disableShadow disabled />
+
+      {/* <button>to disable</button> */}
+      {/* <Button bg="none" border="1px solid grey" color="#000000" /> */}
+
+    </form>
   )
 }
 
