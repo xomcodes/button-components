19c6f375389ed5415563a6f6f024d17962b5b427
Adekunle Babatunde <babatundeadebowale1998@gmail.com>
2023-04-18 16:42:38 +0100

    updates

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,35 @@
-import React from 'react'
-import Sidebar from '../components/sidebar'
-import Button from '@/components/Button'
+import React from "react";
+import Sidebar from "../components/sidebar";
+import Button from "@/components/Button";
 
 function index() {
   return (
-    <form style={{display: "flex", flexDirection: "column", gap: "20px", alignItems: "start"}}>
+    <form
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: "20px",
+        alignItems: "start",
+      }}
+    >
       <Button disableShadow />
       <Button disableShadow disabled />
+      <Button variant="text" disabled />
+
+      <Button startIcon="ic:sharp-add-shopping-cart" />
+      <Button endIcon="ic:sharp-add-shopping-cart" />
+      <Button size="sm" />
+      <Button size="md" />
+      <Button size="lg" />
+      <Button color="default" />
+      <Button color="secondary" />
+      <Button color="danger" />
+      <Button color="primary" />
 
       {/* <button>to disable</button> */}
       {/* <Button bg="none" border="1px solid grey" color="#000000" /> */}
-
     </form>
-  )
+  );
 }
 
-export default index
+export default index;
